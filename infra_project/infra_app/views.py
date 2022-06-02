503eba3041381fac07d38fbc313fca1beb8bcccd
Артём Ларюхин <artimon04@yandex.ru>
2022-06-02 16:40:29 +0300

    add deploy drom docker to 51.250.17.12

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня получилось! На серваке 51.250.17.12 через контейнер Докер!! :))) ЮЮЮЮПППИИИИИ')
 
 
 def second_page(request):
